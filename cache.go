973e577cff3d616e9acafaf0b52f2214c41dd51d
David Sauer <david.sauer@turbinekreuzberg.com>
2018-06-06 15:51:28 +0200

    improved package interface to allow for closing the created cleanup go routine

--- cache.go
+++ cache.go
@@ -10,12 +10,14 @@ type Cache struct {
 	mutex     sync.RWMutex
 	ttl       time.Duration
 	items     map[K]*Item
-	zeroValue Z
+	zeroValue V
 }
 
+// K is the type of the keys
 type K interface{}
+
+// V is the type of the values
 type V interface{}
-type Z interface{}
 
 // Set is a thread-safe way to add new items to the map
 func (cache *Cache) Set(key K, data V) {
@@ -62,29 +64,26 @@ func (cache *Cache) cleanup() {
 	cache.mutex.Unlock()
 }
 
-func (cache *Cache) startCleanupTimer() {
-	duration := cache.ttl
-	if duration < time.Second {
-		duration = time.Second
-	}
-	ticker := time.Tick(duration)
+// StartCleanupTimer creates a goroutine to cleanup old entries in the background
+func (cache *Cache) StartCleanupTimer(interval time.Duration) *time.Ticker {
+	ticker := time.NewTicker(interval)
 	go (func() {
 		for {
 			select {
-			case <-ticker:
+			case <-ticker.C:
 				cache.cleanup()
 			}
 		}
 	})()
+	return ticker
 }
 
 // NewCache is a helper to create instance of the Cache struct
-func NewCache(duration time.Duration, z Z) *Cache {
+func NewCache(duration time.Duration, z V) *Cache {
 	cache := &Cache{
 		ttl:       duration,
 		items:     map[K]*Item{},
 		zeroValue: z,
 	}
-	cache.startCleanupTimer()
 	return cache
 }
--- cache_test.go
+++ cache_test.go
@@ -1,16 +1,14 @@
-package ttlcache
+package ttlcache_test
 
 import (
 	"testing"
 	"time"
+
+	. "github.com/damoon/ttlcache"
 )
 
 func TestGet(t *testing.T) {
-	cache := &Cache{
-		ttl:       time.Second,
-		items:     map[K]*Item{},
-		zeroValue: "",
-	}
+	cache := NewCache(time.Second, "")
 
 	data, exists := cache.Get("hello")
 	if exists || data != "" {
@@ -28,11 +26,7 @@ func TestGet(t *testing.T) {
 }
 
 func TestInt(t *testing.T) {
-	cache := &Cache{
-		ttl:       time.Second,
-		items:     map[K]*Item{},
-		zeroValue: 0,
-	}
+	cache := NewCache(time.Second, 0)
 
 	data, exists := cache.Get(1)
 	if exists || data != 0 {
@@ -50,60 +44,62 @@ func TestInt(t *testing.T) {
 }
 
 func TestExpiration(t *testing.T) {
-	cache := &Cache{
-		ttl:       time.Second,
-		items:     map[K]*Item{},
-		zeroValue: "",
-	}
+	cache := NewCache(time.Second, "")
 
 	cache.Set("x", "1")
 	cache.Set("y", "z")
 	cache.Set("z", "3")
-	cache.startCleanupTimer()
 
-	count := cache.Count()
-	if count != 3 {
-		t.Errorf("Expected cache to contain 3 items")
+	item, exists := cache.Get("x")
+	if !exists || item != "1" {
+		t.Errorf("Expected `x` to not have expired after 500ms")
 	}
-
-	<-time.After(500 * time.Millisecond)
-	cache.mutex.Lock()
-	cache.items["y"].touch(time.Second)
-	item, exists := cache.items["x"]
-	cache.mutex.Unlock()
-	if !exists || item.data != "1" || item.expired() {
-		t.Errorf("Expected `x` to not have expired after 200ms")
-	}
-
-	<-time.After(time.Second)
-	cache.mutex.RLock()
-	_, exists = cache.items["x"]
-	if exists {
-		t.Errorf("Expected `x` to have expired")
-	}
-	_, exists = cache.items["z"]
-	if exists {
-		t.Errorf("Expected `z` to have expired")
-	}
-	_, exists = cache.items["y"]
+	_, exists = cache.Get("y")
 	if !exists {
 		t.Errorf("Expected `y` to not have expired")
 	}
-	cache.mutex.RUnlock()
+	_, exists = cache.Get("z")
+	if !exists {
+		t.Errorf("Expected `z` to not have expired")
+	}
+	count := cache.Count()
+	if count != 3 {
+		t.Errorf("Expected cache to contain 3 item")
+	}
 
+	timer := cache.StartCleanupTimer(time.Second)
+	defer timer.Stop()
+
+	<-time.After(500 * time.Millisecond)
+	item, exists = cache.Get("x")
+	if !exists || item != "1" {
+		t.Errorf("Expected `x` to not have expired after 500ms")
+	}
+
+	<-time.After(600 * time.Millisecond)
+
+	item, exists = cache.Get("x")
+	if !exists || item != "1" {
+		t.Errorf("Expected `x` to not have expired")
+	}
+	_, exists = cache.Get("y")
+	if exists {
+		t.Errorf("Expected `y` to have expired")
+	}
+	_, exists = cache.Get("z")
+	if exists {
+		t.Errorf("Expected `z` to have expired")
+	}
 	count = cache.Count()
 	if count != 1 {
 		t.Errorf("Expected cache to contain 1 item")
 	}
 
-	<-time.After(600 * time.Millisecond)
-	cache.mutex.RLock()
-	_, exists = cache.items["y"]
+	<-time.After(2 * time.Second)
+	_, exists = cache.Get("x")
 	if exists {
-		t.Errorf("Expected `y` to have expired")
+		t.Errorf("Expected `x` to have expired")
 	}
-	cache.mutex.RUnlock()
-
 	count = cache.Count()
 	if count != 0 {
 		t.Errorf("Expected cache to be empty")
